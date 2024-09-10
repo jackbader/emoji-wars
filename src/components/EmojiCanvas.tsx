92b30acfeecfa165dc8a26120afd45c650f05997
Jack Bader <jack@culpritunderwear.com>
2024-09-10 17:25:11 -0400

    adding env variable for database url

--- src/components/EmojiCanvas.tsx
+++ src/components/EmojiCanvas.tsx
@@ -1,7 +1,7 @@
 import { FC, useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io(import.meta.env.VITE_SOCKET_SERVER_URL);
 
 interface EmojiCanvasProps {
   selectedEmoji: string;
